b4043a25f1d9df36b29bbdecf02d984134a462d3
Your Name <you@example.com>
2024-05-16 20:40:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -65,7 +65,10 @@ int is_final(Node* n){
    }
     return 1;
 }
-
+Node* DFS(Node* initial, int* cont){
+   return NULL;
+}
+   
 /*
 Node* DFS(Node* initial, int* cont){
    Stack* pila = createStack();
