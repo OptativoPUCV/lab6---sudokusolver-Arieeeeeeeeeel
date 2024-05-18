426dd7429b9d1094c0b44a82968d85c993064695
Your Name <you@example.com>
2024-05-18 16:42:54 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -69,7 +69,7 @@ List* get_adj_nodes(Node* n){
    List* list=createList();
 
    for (int i = 0; i < 9; i++){
-      for (int k = 0; k < 9; i++){
+      for (int k = 0; k < 9; k++){
          if (n->sudo[i][k] == 0){
             for (int j = 1; j <= 9; j++){
                Node *nuevoNodo = createNode();
